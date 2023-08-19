0e7cfcb34f7f44ab1b88c72948431995dfa31f95
gcc20200225 <99455391+gcc20200225@users.noreply.github.com>
2023-08-19 14:07:14 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -4,15 +4,10 @@ obj.subscription = [{
   "code" : 0,
   "message" : "",
   "data" : {
-    "errormsg" : "成功",
-    "data" : {
-      "stockid" : "17854775",
-      "couponid" : "48697321629",
-      "openid" : "oP_Ic0UbYmljV40YbLLUivM7sYRI"
-    },
-    "issuccess" : true
+
   },
-  "now" : 1692417661000
-}],
+  "now" : 1692428496207
+}
+],
 
 $done({body: JSON.stringify(obj)});
