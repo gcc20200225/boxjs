93c2ee3f6abba07f1792bb557410b0e34d6d2008
gcc20200225 <99455391+gcc20200225@users.noreply.github.com>
2023-08-19 09:11:21 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,9 +1,18 @@
-let obj = JSON.parse($request.body);
+let obj = JSON.parse($response.body);
 
 obj.subscription = [{
-    "openid": "oP_Ic0UbYmljV40YbLLUivM7sYRI",
-    "assemblyid": "64dc89fefc8b470006421fb8",
-    "assemblykey": "paymentchannelcoupon"
-  }],
+  "code" : 0,
+  "message" : "",
+  "data" : {
+    "errormsg" : "成功",
+    "data" : {
+      "stockid" : "17854775",
+      "couponid" : "48697321629",
+      "openid" : "oP_Ic0UbYmljV40YbLLUivM7sYRI"
+    },
+    "issuccess" : true
+  },
+  "now" : 1692417661000
+}],
 
 $done({body: JSON.stringify(obj)});
